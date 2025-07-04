e58479ceda3406b9f72efe49f0ecd1d865572628
jominhyeong97 <jumbo0303@daum.net>
2025-07-04 16:54:30 +0900

    7월4일

---
+++ js/myJs.js
@@ -0,0 +1,7 @@
+function updateF3() {
+            document.getElementById("f1").innerHTML="js 로드순서1";
+        }
+    
+function updateF4() {
+            document.getElementById("f2").innerHTML="js 로드순서2";
+        }
